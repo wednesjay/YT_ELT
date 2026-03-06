e9f2b255b76b509a9f9fba4d6e79fad850fcd5c9
Jashan Garnes <jae@JaeBook-Pro.local>
2026-03-06 13:29:30 -0500

    defined video data extraction function

--- video_stats.py
+++ video_stats.py
@@ -8,6 +8,7 @@ load_dotenv(dotenv_path='./.env')
 
 API_KEY = os.getenv("API_KEY")
 CHANNEL_HANDLE = 'MrBeast'
+maxResults = 50
 
 def get_playlist_id():
 
@@ -26,12 +27,102 @@ def get_playlist_id():
 
         channel_playlistId = channel_items["contentDetails"]["relatedPlaylists"]['uploads']
 
-        print(channel_playlistId)
+        #print(channel_playlistId)
 
         return channel_playlistId
 
     except requests.exceptions.RequestException as e:
         raise e
     
+
+def get_video_ids(playlistId):
+
+    video_ids = []
+
+    pageToken = None
+
+    base_url = f'https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlistId}&key={API_KEY}'
+
+    try:
+
+        while True:
+
+            url = base_url
+
+            if pageToken:
+                url += f'&pageToken={pageToken}'
+
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+
+            pageToken = data.get('nextPageToken')
+
+            if not pageToken:
+                    break
+
+        return video_ids
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
+def extract_video_data(video_ids):
+    extracted_data = []
+
+    def batch_list(video_id_list, batch_size):
+        for video_id in range(0, len(video_id_list), batch_size):
+            yield video_id_list[video_id: video_id + batch_size]
+
+    try: 
+        for batch in batch_list(video_ids, maxResults):
+            video_ids_str = ",".join(batch)
+
+            url = f'https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_ids_str}&key={API_KEY}'
+
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_id = item['id']
+                snippet = item['snippet']
+                contentDetails = item['contentDetails']
+                statistics = item['statistics']
+
+                video_data = {
+                    'video_id': video_id,
+                    'title': snippet['title'],
+                    'publishedAt': snippet['publishedAt'],
+                    'duration': contentDetails['duration'],
+                    'viewCount': statistics.get('viewCount', None),
+                    'likeCount': statistics.get('likeCount', None),
+                    'commentCount': statistics.get('commentCount', None)
+                }
+
+                extracted_data.append(video_data)
+
+        return extracted_data
+            
+    except requests.exceptions.RequestException as e:
+        raise e
+
 if __name__ == '__main__':
-    get_playlist_id()
+    playlistId = get_playlist_id()
+    print('Uploads playlist ID:', playlistId)
+
+    video_ids = get_video_ids(playlistId)
+    print(f'Total videos collected: {len(video_ids)}')
+
+    extract_video_data(video_ids)
+    
+
+
+    
